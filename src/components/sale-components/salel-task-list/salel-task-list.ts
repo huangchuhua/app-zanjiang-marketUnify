67f54f4500bca1eab3fa6ff5ddbc59c5df4e2f35
huangchuhua <huangchweb@163.com>
2018-04-28 10:15:27 +0800

    营销任务列表的收展效果

--- src/components/sale-components/salel-task-list/salel-task-list.ts
+++ src/components/sale-components/salel-task-list/salel-task-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChildren,ElementRef,Renderer,QueryList} from '@angular/core';
 
 /**
  * Generated class for the SalelTaskListComponent component.
@@ -12,11 +12,30 @@ import { Component } from '@angular/core';
 })
 export class SalelTaskListComponent {
 
-  text: string;
+  @ViewChildren('list') listEle:QueryList<ElementRef>;
 
-  constructor() {
-    console.log('Hello SalelTaskListComponent Component');
-    this.text = 'Hello World';
+  constructor(el: ElementRef, renderer: Renderer) {
   }
-
+  //显示与隐藏更多字段点击事件
+  upAndDown(id){
+    let itemArr=this.listEle.last.nativeElement.children;
+    let arrow_btn=itemArr[id].children[0].children[1];
+    let content=itemArr[id].children[1];
+    console.log(arrow_btn,content);
+    
+    if(arrow_btn.classList.contains("arrow_down")){
+      arrow_btn.classList.remove("arrow_down");
+      arrow_btn.classList.add("arrow_up");
+      //改变字段的高度
+      content.style.height="auto";
+    }else{
+      arrow_btn.classList.remove("arrow_up");
+      arrow_btn.classList.add("arrow_down");
+      //改变字段的高度
+      content.style.height="95px";
+    }
+  }
+  ngOnInit(){
+    // console.log(999)
+  }
 }
--- src/pages/salelist/salelist.ts
+++ src/pages/salelist/salelist.ts
@@ -11,5 +11,5 @@ export class SalelistPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  
 }
